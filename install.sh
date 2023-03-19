df76c9d8adbad47c8207cb7a8b9d4dee17040bda
k3nny <niklas@baak.xyz>
2023-03-19 19:57:10 +0100

    root prevention in install

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ "$EUID" -eq 0 ]
+    then
+        echo "do not use as root"
+        exit
+fi
+
+
 sudo apt install nvidia-cuda-toolkit python3-pip python3-opencv swig 
 
 # ubuntu 22.04+
@@ -14,4 +21,4 @@ python3 -m pip install gym install gym[all] torch torchvision torchaudio
 python3 -m pip install stable-baselines3[extra]
 
 # in the future just pyglet
-python3 -m pip instal pyglet==1.5.27
+python3 -m pip install pyglet==1.5.27
